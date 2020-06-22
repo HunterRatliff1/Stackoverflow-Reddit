fa4d88ace38c3c619795f9e1fbdb9eeb5760cca7
Hunter Ratliff <Hunterratliff1@gmail.com>
2020-06-22 09:30:57 -0500

    Reddit question
    https://www.reddit.com/r/rstats/comments/hdry5p/r_end_user_gui/

---
+++ coworker script.R
@@ -0,0 +1,12 @@
+coworker_helper <- function(...){
+  
+  ## Use rstudioapi to select files / directory
+  path_dir  <- rstudioapi::selectDirectory(caption = "Pick the folder that contains the files")
+  path_file <- rstudioapi::selectFile(caption = "Select the new file")
+  
+  ## your script here
+  message("Using directory: path_dir")
+  list.files(path_dir) # return something
+  
+}
+coworker_helper()
